23f8ceed167a3409e8d5ea9b992b0b03efe4f2aa
nhutle8420 <nhutle8420@gmail.com>
2022-11-24 18:02:53 +0700

    Setup API

---
+++ config/database.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func setupDatabase() *gorm.DB {
+	errEvn := godotenv.Load()
+	if errEvn != nil {
+		panic("Faile to load env file env")
+	}
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Fail to create to Connect Database")
+	}
+	return db
+}
+func closeConnect(db *gorm.DB) {
+	dbSql, err := db.DB()
+	if err !=nil {
+		panic("Faile to close database")
+	}
+	dbSql.Close() 
+}
+
